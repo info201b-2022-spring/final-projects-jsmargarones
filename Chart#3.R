57bc60708c6640fcb939e36aeb21b58577fcdce9
quinnor2 <qorosenberg@gmail.com>
2022-05-20 12:30:23 -0700

    Summary and Chart 3

---
+++ Chart#3.R
@@ -0,0 +1,58 @@
+library(ggplot2)
+library(maps)
+library(dplyr)
+library(viridis)
+library(plotly)
+
+#load the dataset
+auto_df <- read.csv("/Users/quinnrosenberg/final-projects-jsmargarones/Largest automakers by market capitalization.csv")
+
+#load the map
+world_map <- map('world',col="darkgrey", fill=TRUE, bg="white", lwd=0.05, mar=rep(0,4),border=0, ylim=c(-80,80) )
+
+#find the total market cap for each country in billions of dollars
+by_country <- auto_df %>% 
+  select(price..USD., country) %>% 
+  group_by(country) %>% 
+  summarize(mean_price = mean(price..USD.)) %>%
+  arrange(-mean_price)
+
+#download dataset with latitude and longitude of each country
+lat_and_long <- read.csv("/Users/quinnrosenberg/Downloads/world_country_and_usa_states_latitude_and_longitude_values.csv")
+lat_and_long <- select(lat_and_long, country, latitude, longitude)
+
+#merge the datasets by country
+by_country <- merge(x=by_country, y=lat_and_long, by="country")
+
+#reorder data
+by_country <- by_country %>%
+  arrange(-mean_price) %>%
+  mutate(mytext = paste(
+  "Country: ", country, "\n", 
+  "Average Stock Price: $", mean_price, sep="")
+  ) %>%
+  mutate(mean_price = round(mean_price, digits = 2))
+
+#create bubble map plot
+average_stock_price_by_country <- ggplot() +
+  geom_polygon(data = world_map, aes(x = long, y = lat, group = group), alpha = 0.03) +
+  geom_point(data = by_country, aes(x = longitude, y = latitude, size = mean_price, color = mean_price, text = mytext, alpha = mean_price)) +
+  scale_size_continuous(range = c(1,14)) +
+  scale_color_viridis(option = "inferno", trans ="log") +
+  scale_alpha_continuous(trans="log") +
+  theme_void() +
+  theme(legend.position = "left") +
+  title("Average Stock Price by Country")
+
+#Get interactive bubble map(must look in 'Viewer' not 'Plots')
+stock_plot <- ggplotly(average_stock_price_by_country, tooltip = "text")
+
+
+
+
+
+
+
+
+  
+
---
+++ summary_information.R
@@ -0,0 +1,86 @@
+library(dplyr)
+library(stringr)
+
+tesla_stock_df <- read.csv("/Users/quinnrosenberg/final-projects-jsmargarones/TSLA.csv")
+  
+summary_info <- list()
+
+# A function that takes in a dataset and returns a list of info about it
+summary_info$num_observations <- nrow(tesla_stock_df)
+summary_info$num_variables <- ncol(tesla_stock_df)
+
+#On what date was the maximum price the highest?
+summary_info$max_date <- tesla_stock_df %>%
+  filter(High == max(High, na.rm = T)) %>%
+  select(Date, High)
+
+#How many total units have been traded?
+summary_info$total_units_sold <- tesla_stock_df %>%
+  select(Volume) %>%
+  summarise(total_units_sold = sum(Volume))
+
+#What is the average (mean) number of units sold in a day?
+summary_info$mean_volume <- tesla_stock_df %>%
+  select(Volume) %>%
+  summarise(mean_volume = mean(Volume))
+
+#What is the largest price jump in a day?
+summary_info$max_price_jump <- tesla_stock_df %>%
+  mutate(price_change = Close - Open) %>%
+  filter(price_change == max(price_change, na.rm = T)) %>%
+  select(Date, price_change, Open, Close)
+
+#What was the largest price drop in a day?
+summary_info$max_price_drop <- tesla_stock_df %>%
+  mutate(price_change = Close - Open) %>%
+  filter(price_change == min(price_change, na.rm = T)) %>%
+  select(Date, price_change, Open, Close)
+
+#In what year did the price change the most?
+summary_info$max_change_year <- tesla_stock_df %>%
+  mutate(Year = str_sub(Date, 1, 4)) %>%
+  group_by(Year) %>%
+  filter(Date == max(Date) | Date == min(Date)) %>% 
+  summarise(.groups = "keep", price_change = (max(High) - min(Low)), Year) %>%
+  distinct(price_change) %>%
+  ungroup() %>%
+  filter(price_change == max(price_change))
+
+#In what year did the price change the least?
+summary_info$min_change_year <- tesla_stock_df %>%
+  mutate(Year = str_sub(Date, 1, 4)) %>%
+  group_by(Year) %>%
+  filter(Date == max(Date) | Date == min(Date)) %>% 
+  summarise(.groups = "keep", price_change = (max(High) - min(Low)), Year) %>%
+  distinct(price_change) %>%
+  ungroup() %>%
+  filter(price_change == min(price_change))
+
+#In what year were the most units sold?
+summary_info$max_volume_year <- tesla_stock_df %>%
+  mutate(Year = str_sub(Date, 1, 4)) %>%
+  group_by(Year) %>%
+  summarise(Volume = sum(Volume), Year) %>%
+  distinct(Volume) %>%
+  ungroup() %>%
+  filter(Volume == max(Volume))
+
+#In what year were the least units sold?
+summary_info$min_volume_year <- tesla_stock_df %>%
+  mutate(Year = str_sub(Date, 1, 4)) %>%
+  group_by(Year) %>%
+  summarise(Volume = sum(Volume), Year) %>%
+  distinct(Volume) %>%
+  ungroup() %>%
+  filter(Volume == min(Volume))
+
+
+
+
+  
+  
+         
+
+
+
+
